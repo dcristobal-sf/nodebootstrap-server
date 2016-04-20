f9e05a5a5882e9e52eece4b09ceb4a1e09be2fa1
Christabella <christabella.irwanto@gmail.com>
2016-04-21 00:13:09 +0800

    Update app.js for openshift as well

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ exports.setup = function(initapp, callback) {
 
   if (is_http_thread) {
     http = http.createServer(app);
-    http.listen(process.env.PORT || CONF.app.port);
+    http.listen(process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || CONF.app.port);
   }
 
   // If we are not running a cluster at all:
